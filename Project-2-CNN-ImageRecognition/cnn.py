fd1cda3cd10244fbf3362664676abcb7344212ab
rohit517 <rohithkrishna517@gmail.com>
2017-12-26 14:03:00 -0500

    Update

--- Project-2-CNN-ImageRecognition/cnn.py
+++ Project-2-CNN-ImageRecognition/cnn.py
@@ -64,7 +64,7 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',
 
 classifier.fit_generator(training_set,
                          steps_per_epoch = 8000,
-                         epochs = 25,
+                         epochs = 1,
                          validation_data = test_set,
                          validation_steps = 2000)
 
